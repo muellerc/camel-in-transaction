b5cb718e6caac22c9bff64398b365fcfeb91c302
Christian Mueller <christian.mueller@gmail.com>
2012-05-03 00:04:22 +0200

    updates Atomikos to 3.7.1, updated JOTM to 2.2.3, enabled recovery for JOTM

--- src/test/java/org/apache/cmueller/camel/samples/camelone/xa/JmsAndJdbcXATransactionSampleWithJOTMTest.java
+++ src/test/java/org/apache/cmueller/camel/samples/camelone/xa/JmsAndJdbcXATransactionSampleWithJOTMTest.java
@@ -1,9 +1,21 @@
 package org.apache.cmueller.camel.samples.camelone.xa;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class JmsAndJdbcXATransactionSampleWithJOTMTest extends BaseJmsAndJdbcXATransactionSampleTest {
+	
+    @BeforeClass
+    public static void setUpBeforeClass() {
+    	System.setProperty("jotm.home", "src/test/resources");
+    }
+    
+    @AfterClass
+    public static void tearDownAfterClass() {
+    	System.clearProperty("jotm.home");
+    }
 
     @Override
     protected AbstractApplicationContext createApplicationContext() {
