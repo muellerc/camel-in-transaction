9f42df2586aa4e60e4accfd7770bf291e862de7c
James Carman <james@carmanconsulting.com>
2012-09-16 22:28:19 -0400

    Example with "quasi" transactions.

---
+++ src/test/java/org/apache/cmueller/camel/samples/camelone/quasi/JmsAndJdbcQuasiTransactionSampleTest.java
@@ -0,0 +1,143 @@
+package org.apache.cmueller.camel.samples.camelone.quasi;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.test.junit4.CamelSpringTestSupport;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+public class JmsAndJdbcQuasiTransactionSampleTest extends CamelSpringTestSupport
+{
+    private JdbcTemplate jdbc;
+    private TransactionTemplate transactionTemplate;
+//----------------------------------------------------------------------------------------------------------------------
+// Other Methods
+//----------------------------------------------------------------------------------------------------------------------
+
+    @Override
+    protected AbstractApplicationContext createApplicationContext()
+    {
+        return new ClassPathXmlApplicationContext("META-INF/spring/JmsAndJdbcQuasiTransactionSampleTest-context.xml");
+    }
+
+    @Before
+    @Override
+    public void setUp() throws Exception
+    {
+        super.setUp();
+
+        DataSource ds = context.getRegistry().lookup("dataSource", DataSource.class);
+        jdbc = new JdbcTemplate(ds);
+
+        PlatformTransactionManager transactionManager = context.getRegistry().lookup("transactionManager", PlatformTransactionManager.class);
+        transactionTemplate = new TransactionTemplate(transactionManager);
+
+        transactionTemplate.execute(new TransactionCallbackWithoutResult()
+        {
+            @Override
+            protected void doInTransactionWithoutResult(TransactionStatus status)
+            {
+                jdbc.execute("CREATE TABLE account (name VARCHAR(50), balance BIGINT)");
+                jdbc.execute("INSERT INTO account VALUES('foo',1000)");
+                jdbc.execute("INSERT INTO account VALUES('bar',1000)");
+            }
+        });
+    }
+
+    @After
+    @Override
+    public void tearDown() throws Exception
+    {
+        transactionTemplate.execute(new TransactionCallbackWithoutResult()
+        {
+            @Override
+            protected void doInTransactionWithoutResult(TransactionStatus status)
+            {
+                jdbc.execute("DROP TABLE account");
+            }
+        });
+
+        super.tearDown();
+    }
+
+
+    @Override
+    protected RouteBuilder createRouteBuilder() throws Exception
+    {
+        return new RouteBuilder()
+        {
+            @Override
+            public void configure() throws Exception
+            {
+                from("jms:input.one")
+                        .transacted()
+                        .to("sql:UPDATE account SET balance = (SELECT balance from account where name = 'foo') - # WHERE name = 'foo'?dataSourceRef=dataSource")
+                        .to("sql:UPDATE account SET balance = (SELECT balance from account where name = 'bar') + # WHERE name = 'bar'?dataSourceRef=dataSource")
+                        .to("jms:output.one");
+
+                from("jms:input.two")
+                        .transacted()
+                        .to("sql:UPDATE account SET balance = (SELECT balance from account where name = 'foo') - # WHERE name = 'foo'?dataSourceRef=dataSource")
+                        .throwException(new SQLException("forced exception for test"))
+                        .to("sql:UPDATE account SET balance = (SELECT balance from account where name = 'bar') + # WHERE name = 'bar'?dataSourceRef=dataSource")
+                        .to("jms:output.two");
+
+            }
+        };
+    }
+
+    private long queryForLong(final String query)
+    {
+        return transactionTemplate.execute(new TransactionCallback<Long>()
+        {
+            @Override
+            public Long doInTransaction(TransactionStatus status)
+            {
+                return jdbc.queryForLong(query);
+            }
+        });
+    }
+
+
+    @Test
+    public void moneyShouldTransfer()
+    {
+        assertEquals(1000, queryForLong("SELECT balance from account where name = 'foo'"));
+        assertEquals(1000, queryForLong("SELECT balance from account where name = 'bar'"));
+
+        template.sendBody("jms:input.one", 100L);
+
+        Exchange exchange = consumer.receive("jms:output.one", 5000);
+        assertNotNull(exchange);
+
+        assertEquals(900, queryForLong("SELECT balance from account where name = 'foo'"));
+        assertEquals(1100, queryForLong("SELECT balance from account where name = 'bar'"));
+    }
+
+    @Test
+    public void moneyShouldNotTransfer()
+    {
+        assertEquals(1000, queryForLong("SELECT balance from account where name = 'foo'"));
+        assertEquals(1000, queryForLong("SELECT balance from account where name = 'bar'"));
+
+        template.sendBody("jms:input.two", 100L);
+
+        Exchange exchange = consumer.receive("jms:ActiveMQ.DLQ", 5000);
+        assertNotNull(exchange);
+
+        assertEquals(1000, queryForLong("SELECT balance from account where name = 'foo'"));
+        assertEquals(1000, queryForLong("SELECT balance from account where name = 'bar'"));
+    }
+}
